cec11b59928fa44f10bf64f530c8728ef5b200eb
Fixingwoo <fixingwoo@gmail.com>
2021-08-22 02:08:04 +0900

    feat: add to Redirect, Switch

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Detail from 'Routes/Detail';
@@ -8,12 +8,13 @@ import Search from 'Routes/Search';
 export default () => {
     return (
         <Router>
-            <>
+            <Switch>
                 <Route path="/" exact component={ Home } />
                 <Route path="/tv" component={ TV } />
                 <Route path="/detail" component={ Detail } />
                 <Route path="/search" component={ Search } />
-            </>
+                <Redirect from="*" to="/" />
+            </Switch>
         </Router>
     )
 };
