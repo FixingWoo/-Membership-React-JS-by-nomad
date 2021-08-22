a1e5ca1fbea0314fc52c4fd2851ea3f5b697a297
Fixingwoo <fixingwoo@gmail.com>
2021-08-22 20:15:56 +0900

    design: GlobalStyles + Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import Header from 'Components/Header';
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Detail from 'Routes/Detail';
@@ -8,13 +10,16 @@ import Search from 'Routes/Search';
 export default () => {
     return (
         <Router>
-            <Switch>
-                <Route path="/" exact component={ Home } />
-                <Route path="/tv" component={ TV } />
-                <Route path="/detail" component={ Detail } />
-                <Route path="/search" component={ Search } />
-                <Redirect from="*" to="/" />
-            </Switch>
+            <>
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={ Home } />
+                    <Route path="/tv" component={ TV } />
+                    <Route path="/detail" component={ Detail } />
+                    <Route path="/search" component={ Search } />
+                    <Redirect from="*" to="/" />
+                </Switch>
+            </>
         </Router>
     )
 };
